da86b908a9d93b73d87a2a06d82a7ac603df0840
Alexandra Marin <alexandra.marin01@gmail.com>
2014-08-13 00:56:02 +0300

    Normal chain

--- MovieRental/CalculatorChain.cs
+++ MovieRental/CalculatorChain.cs
@@ -3,25 +3,62 @@ using System.Collections.Generic;
 
 namespace MovieRental
 {
-
-	public class NormalCalculatorChain
+	public class CalculatorChain
 	{
 		int cost;
 		int days;
+		int discountCost;
+		int afterDays;
 
 		public void BaseCost(int cost)
 		{
-			cost = cost;
+			this.cost = cost;
 		}
 
-		public int AfterDays(int days)
+		public int ForDays(int days)
 		{
-			days = days;
+			this.days = days;
+		}
+
+		public int ApplyDiscount(int discount)
+		{
+			this.discountCost = discount;
+		}
+
+		public int AfterDays(int afterDays)
+		{
+			this.afterDays = afterDays;
 		}
 
 		public int Calculate()
 		{
-			return cost * days;
+			bool discountApplies = days > afterDays;
+
+			if (discountApplies) {
+				return cost * (days - afterDays + 1) + discountCost * (afterDays - 1); //full price for the first days, discounted after
+			} 
+			else 
+			{
+				return cost * days;
+			}
+		}
+	}
+
+	public class NormalCalculatorChain
+	{
+		private CalculatorChain calculator;
+
+		public NormalCalculatorChain()
+		{
+			calculator = new CalculatorChain ();
+			calculator.BaseCost (2);
+			calculator.AfterDays (2);
+			calculator.ApplyDiscount (1);
+		}
+
+		public int ForDays(int days)
+		{
+			calculator.ForDays(days);
 		}
 	}
 }
