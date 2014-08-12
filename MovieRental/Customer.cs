8fc20eb08f142ef04669f01af77fd5d0a5ac5f00
Alexandra Marin <alexandra.marin01@gmail.com>
2014-08-12 23:04:47 +0300

    Made a view. :|

--- MovieRental/Customer.cs
+++ MovieRental/Customer.cs
@@ -8,28 +8,6 @@ namespace MovieRental
 		public string Name {get; set;}
 		public List<Rental> Rentals { get; set;}
 		int LoyalityPoints = 0;
-
-		public Customer ()
-		{
-		}
-
-		public void ShowSummary()
-		{
-			int total = 0;
-
-			Console.WriteLine ("Calculate for customer...");
-
-			foreach (var rental in Rentals) 
-			{
-				total += rental.CalculatePrice ();
-				Console.WriteLine ("Rental cost is...");
-
-				LoyalityPoints = rental.CalculatePoints ();
-				Console.WriteLine ("Total points for rental is...");
-			}
-
-			Console.WriteLine ("Total rent is...");
-		}
 	}
 }
 
