ca789f7abf8d4aa14de570316a4eb009a8a94205
Alexandra Marin <alexandra.marin01@gmail.com>
2014-08-17 14:57:43 +0300

    Add tests and mock

---
+++ MovieRentalTests/Customer Tests/WithPopularRentals.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using System.Collections.Generic;
+using MovieRental;
+using MovieRentalTests;
+using NUnit.Framework;
+
+namespace CustomerTests
+{
+    [TestFixture ()]
+    public class WithPopularRentals
+    {
+        Customer customer;
+        CustomerController controller;
+
+        [SetUpAttribute]
+        public void DefineCustomer ()
+        {
+            customer = new Customer ();
+            customer.LoyalityPoints = 0;
+
+            customer.Rentals = new List<IPurchase> (); 
+            customer.Rentals.Add ((IPurchase)(new Mocks().MockRental.MockInstance)); 
+            customer.Rentals.Add ((IPurchase)(new Mocks().MockPurchase.MockInstance));
+
+            controller = new CustomerController(customer);
+        }
+
+        [Test ()]
+        public void PaysMoreForPopularRental ()
+        {
+            Assert.IsTrue (true);
+        }
+
+        [Test ()]
+        public void DoesntGetPointsForPopularRentals ()
+        {
+            Assert.IsTrue (true);
+        }
+    }
+}
--- MovieRentalTests/Mocks/Mocks.cs
+++ MovieRentalTests/Mocks/Mocks.cs
@@ -6,6 +6,7 @@ namespace MovieRentalTests
     public class Mocks
     {
         public DynamicMock MockRental = new DynamicMock(typeof(IPurchase));
+        public DynamicMock MockPopularRental = new DynamicMock(typeof(IPurchase));
         public DynamicMock MockPurchase = new DynamicMock(typeof(IPurchase));
 
         public Mocks()
@@ -14,6 +15,11 @@ namespace MovieRentalTests
             MockRental.ExpectAndReturn("get_Price", PriceCode.Kids);
             MockRental.ExpectAndReturn("get_Days", 10);
             MockRental.ExpectAndReturn("get_Type", PurchaseType.Rental);
+
+            //Rental
+            MockPopularRental.ExpectAndReturn("get_Price", PriceCode.Kids);
+            MockPopularRental.ExpectAndReturn("get_Days", 10);
+            MockPopularRental.ExpectAndReturn("get_Type", PurchaseType.Rental);
 
             //Purchase
             MockPurchase.ExpectAndReturn("get_Price", PriceCode.Premiere);
